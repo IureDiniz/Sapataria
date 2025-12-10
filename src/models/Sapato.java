a9b9a922b099978145605455dfa681a556794668
GustavoRV08 <gustavorv102@gmail.com>
2025-12-10 12:10:56 -0300

    Models1
    teste

--- src/models/Sapato.java
+++ src/models/Sapato.java
@@ -9,5 +9,34 @@ package models;
  * @author iured
  */
 public class Sapato {
+    int sap_codigo;
+    String sap_nome;
+    String sap_fornecedor;
+    double sap_preco_compra;
+    double sap_preco_venda;
+    int sap_quantidade;
     
+    boolean existeSapato(int idSapato){
+        /*if(getSapato(idSapato) != null){
+            return true;
+        }else{
+            return false;
+        }
+        */
+        return false;
+    }
+    
+    boolean possuiPedido(){
+        /*
+        ArrayList<Pedido> pedidos = PedidoDAO.listPedido();
+        for(int i =0; i<pedidos.size(); i++){
+            Pedido ped = pedidos.get(i);
+            if(ped.sap_codigo == this.sap_codigo){
+                return true;
+                break;
+            }
+        }
+        */
+        return false;
+    }
 }
