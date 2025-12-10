c6db8e3b22cb48cae8edb26a40f2568a71fcbcff
GustavoRV08 <gustavorv102@gmail.com>
2025-12-10 12:16:08 -0300

    Models2

--- src/models/Pedido.java
+++ src/models/Pedido.java
@@ -11,6 +11,7 @@ package models;
 public class Pedido {
     int ped_codigo;
     String ped_cliente;
+    java.sql.Date ped_data;
     int ped_quantidade;
     double ped_preco_total;
     int sap_codigo;
