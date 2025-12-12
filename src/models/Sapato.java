a8551e563e3c10fb9f92c6e48fdc694409ac2709
GustavoRV08 <gustavorv102@gmail.com>
2025-12-12 14:58:42 -0300

    models3

--- src/models/Sapato.java
+++ src/models/Sapato.java
@@ -4,39 +4,93 @@
  */
 package models;
 
+import static dao.PedidoDAO.listPedido;
+import static dao.SapatoDAO.getSapato;
+import java.util.List;
+
 /**
  *
  * @author iured
  */
 public class Sapato {
-    int sap_codigo;
-    String sap_nome;
-    String sap_fornecedor;
-    double sap_preco_compra;
-    double sap_preco_venda;
-    int sap_quantidade;
+
+    private int SAP_CODIGO;
+    private String SAP_NOME;
+    private String SAP_FORNECEDOR;
+    private double SAP_PRECO_COMPRA;
+    private double SAP_PRECO_VENDA;
+    private int SAP_QUANTIDADE;
     
-    boolean existeSapato(int idSapato){
+    public boolean existeSapato(int idSapato){
+        if(getSapato(idSapato) != null){
+            return true;
+        }else{
+            return false;
+        }
         /*if(getSapato(idSapato) != null){
             return true;
         }else{
             return false;
         }
         */
-        return false;
     }
     
-    boolean possuiPedido(){
-        /*
-        ArrayList<Pedido> pedidos = PedidoDAO.listPedido();
-        for(int i =0; i<pedidos.size(); i++){
-            Pedido ped = pedidos.get(i);
-            if(ped.sap_codigo == this.sap_codigo){
-                return true;
-                break;
-            }
+    public boolean possuiEstoque(int id, int qtde){
+        Sapato sap = getSapato(id);
+        if(sap.getSAP_QUANTIDADE() >= qtde){
+            return true;
+        }else{
+            return false;
         }
-        */
-        return false;
     }
+    
+    public int getSAP_CODIGO() {
+        return SAP_CODIGO;
+    }
+
+    public void setSAP_CODIGO(int SAP_CODIGO) {
+        this.SAP_CODIGO = SAP_CODIGO;
+    }
+
+    public String getSAP_NOME() {
+        return SAP_NOME;
+    }
+
+    public void setSAP_NOME(String SAP_NOME) {
+        this.SAP_NOME = SAP_NOME;
+    }
+
+    public String getSAP_FORNECEDOR() {
+        return SAP_FORNECEDOR;
+    }
+
+    public void setSAP_FORNECEDOR(String SAP_FORNECEDOR) {
+        this.SAP_FORNECEDOR = SAP_FORNECEDOR;
+    }
+
+    public double getSAP_PRECO_COMPRA() {
+        return SAP_PRECO_COMPRA;
+    }
+
+    public void setSAP_PRECO_COMPRA(double SAP_PRECO_COMPRA) {
+        this.SAP_PRECO_COMPRA = SAP_PRECO_COMPRA;
+    }
+
+    public double getSAP_PRECO_VENDA() {
+        return SAP_PRECO_VENDA;
+    }
+
+    public void setSAP_PRECO_VENDA(double SAP_PRECO_VENDA) {
+        this.SAP_PRECO_VENDA = SAP_PRECO_VENDA;
+    }
+
+    public int getSAP_QUANTIDADE() {
+        return SAP_QUANTIDADE;
+    }
+
+    public void setSAP_QUANTIDADE(int SAP_QUANTIDADE) {
+        this.SAP_QUANTIDADE = SAP_QUANTIDADE;
+    }
+    
+    
 }
